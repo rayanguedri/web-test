4e735d3973566974fc443010a657e747f6f7fc6b
rayanguedri <63672654+rayanguedri@users.noreply.github.com>
2022-04-13 13:36:40 +0100

    t

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Publication;
+use App\Entity\Comment;
 use App\Repository\PublicationRepository;
+use App\Repository\CommentRepository;
 use DateTime;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -14,7 +16,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\Id;
+use app\Form\CommentFormType;
 use Symfony\Component\Validator\Constraints\DateTime as ConstraintsDateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class BlogController extends AbstractController
 {
@@ -26,6 +30,7 @@ class BlogController extends AbstractController
 
 
         $publications = $repo->findAll();
+        $comment = $repo->findAll();
 
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
@@ -64,12 +69,16 @@ class BlogController extends AbstractController
             ->add('image')
             ->getForm();
 
+
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             if (!$publication->getId()) {
                 $publication->setCreatedAt(new \DateTimeImmutable());
             }
+
+
 
 
             $manager->persist($publication);
@@ -86,6 +95,51 @@ class BlogController extends AbstractController
     }
 
     /**
+     * @Route("/blog/neww", name="blog_createe")
+     * @Route("/blog/{id}/editt" , name = "blog_edit")
+     */
+    public function create2(Comment $comment = null, Request $request, EntityManagerInterface $manager)
+    {
+
+        
+
+        if (!$comment) {
+
+            $comment = new Comment;
+        }
+
+    
+
+        $form = $this->createFormBuilder($comment)
+            ->add('auteur')
+            ->add('contenu')
+
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$comment->getId()) {
+
+                $comment->setCreatedAt(new \DateTimeImmutable());
+            }
+
+
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $comment->getId()]);
+        }
+
+
+
+        return $this->render('blog/Comment.html.twig', [
+            'formComment' => $form->createView()
+        ]);
+    }
+
+
+    /**
      * @Route("/blog/{id}", name="blog_show")
      */
     public function show(Publication $publication)
@@ -97,15 +151,19 @@ class BlogController extends AbstractController
     }
 
     /**
- * @Route("/blog/show/{id}", name="delete_blog")
- */
-public function delete($id): Response
-{
-    $entityManager = $this->getDoctrine()->getManager();
-    $publication = $entityManager->getRepository(Publication::class)->find($id);
-    $entityManager->remove($publication);
-    $entityManager->flush();
+     * @Route("/blog/show/{id}", name="delete_blog")
+     */
+    public function delete($id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $publication = $entityManager->getRepository(Publication::class)->find($id);
+        $entityManager->remove($publication);
+        $entityManager->flush();
 
-    return $this->redirectToRoute("blog");
-}
+        return $this->redirectToRoute("blog");
+    }
+
+    /**
+     * @Route("/blog/neww", name="blog_createe")
+     */
 }
